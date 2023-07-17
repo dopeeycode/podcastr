764018f7802ec4aeb434a3ea51d4720c24db8d82
Italo <italodopeey@gmail.com>
2023-07-17 20:30:40 -0300

    feat: latest episodes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,86 @@
-import React from 'react'
+import { api } from '@/services/axios'
+import format from 'date-fns/format'
+import ptBR from 'date-fns/locale/pt-BR'
+import { parseISO } from 'date-fns'
 
-export default function index() {
-  return <h1>Index</h1>
+import { convertDurationToTimeString } from '@/utils/convertDurationToTimeString'
+import { GetStaticProps } from 'next'
+
+import styles from './styles.module.scss'
+import LatestEpisodes from '@/components/LatestEpisodes'
+
+export interface EspisodeProps {
+  id: string
+  title: string
+  members: string
+  published_at: string
+  thumbnail: string
+  durationAsString: string
+  description: string
+  file: {
+    duration: number
+    url: string
+  }
+}
+
+interface HomeProps {
+  allEpisodes: EspisodeProps[]
+  latestEpisodes: EspisodeProps[]
+}
+
+export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  return (
+    <div className={styles.homePage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Últimos lançamentos</h2>
+
+        <ul>
+          {latestEpisodes.map((episode) => {
+            return <LatestEpisodes key={episode.id} episode={episode} />
+          })}
+        </ul>
+      </section>
+
+      <section className={styles.allEpisodes}></section>
+    </div>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc',
+    },
+  })
+
+  const episodes = data.map((episode: EspisodeProps) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      published_at: format(parseISO(episode.published_at), 'EEEEEE, d MMMM', {
+        locale: ptBR,
+      }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(
+        Number(episode.file.duration),
+      ),
+      description: episode.description,
+      ul: episode.file.url,
+    }
+  })
+
+  const latestEpisodes = episodes.slice(0, 2)
+  const allEpisodes = episodes.slice(2, episodes.lenght)
+
+  return {
+    props: {
+      latestEpisodes,
+      allEpisodes,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
